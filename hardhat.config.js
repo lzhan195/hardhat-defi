d80af4aaeae66ca316f9a2e54797926d3243a726
lzhan195 <longtengzhang77@gmail.com>
2023-08-21 20:12:46 -0400

    lending pool and weth

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,52 +3,63 @@ require("@nomiclabs/hardhat-etherscan")
 require("hardhat-deploy")
 require("solidity-coverage")
 require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
 require("dotenv").config()
 
 const COINMARKEYCAP_API_KEY = process.env.COINMARKEYCAP_API_KEY
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-      blockConfirmations: 1,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+            blockConfirmations: 1,
+        },
+        sepolia: {
+            chainId: 11155111,
+            blockConfirmations: 6,
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
     },
-    sepolia: {
-      chainId: 11155111,
-      blockConfirmations: 6,
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-    }
-  },
-  etherscan: {
-    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
-      sepolia: ETHERSCAN_API_KEY,
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+        customChains: [],
     },
-    customChains: [],
-  },
-  gasReporter: {
-    enabled: false,
-    currency: "USD",
-    outputFile: "gas-report.txt",
-    noColors: true,
-    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-  },
-  solidity: "0.8.7",
-  namedAccounts: {
-    deployer: {
-      default: 0,
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
-    player: {
-      default: 1,
+    solidity: {
+        compilers: [
+            { version: "0.8.7" },
+            { version: "0.6.0" },
+            { version: "0.6.6" },
+            { version: "0.6.12" },
+            { version: "0.4.19" },
+        ],
     },
-  },
-  mocha: {
-    timeout: 200000,
-  }
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 200000,
+    },
 }
---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,31 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+const { getWeth } = require("../scripts/getWeth")
+
+async function main() {
+    //the protocol treats everything as an ERC20 token
+    await getWeth()
+    const { deployer } = await getNamedAccounts()
+
+    //Lending Pool
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account,
+    )
+
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+    return lendingPool
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
