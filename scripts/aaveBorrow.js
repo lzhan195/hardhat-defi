d045dfb54b67f1cb19dad2e2f02ef1517ca667a7
lzhan195 <longtengzhang77@gmail.com>
2023-08-21 21:50:38 -0400

    borrow dai

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -19,13 +19,20 @@ async function main() {
     console.log("Deposited!")
 
     let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(lendingPool, deployer)
+    //DAI
     const daiPrice = await getDaiPrice()
     const amountDaiToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
     const amountDaiToBorrowWei = ethers.utils.parseEther(amountDaiToBorrow.toString())
-
-    //DAI
-
     //Borrow
+    const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer)
+    await getBorrowUserData(lendingPool, deployer)
+}
+
+async function borrowDai(daiAddress, lendingPool, amountDaiToBorrowWei, account) {
+    const borrowTx = await lendingPool.borrow(daiAddress, amountDaiToBorrowWei, 1, 0, account)
+    await borrowTx.wait(1)
+    console.log("You are borrowed!")
 }
 
 async function getDaiPrice() {
